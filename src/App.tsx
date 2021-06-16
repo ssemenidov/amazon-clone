0f60b4ba8c4fdcc6fc2df462535dade35ef726ff
ssemenidov <23sergey03@gmail.com>
2021-06-16 14:59:06 +0800

    busket check

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,28 @@ import "./App.css";
 import Busket from "./components/Busket/Busket";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import { ThemeProvider } from "styled-components";
 
+const theme = {
+  palette: {
+    primary: "#ffb020",
+  },
+};
 function App() {
   return (
     <div className="app">
       <Router>
-        <Header></Header>
-        <Switch>
-          <Route exact path="/busket">
-            <Busket></Busket>
-          </Route>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-        </Switch>
+        <ThemeProvider theme={theme}>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/busket">
+              <Busket></Busket>
+            </Route>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+          </Switch>
+        </ThemeProvider>
       </Router>
     </div>
   );
--- src/components/Busket/Busket.tsx
+++ src/components/Busket/Busket.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Busket.css";
-import Grid, { GridSpacing } from "@material-ui/core/Grid";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
+import Checkbox from "@material-ui/core/Checkbox";
 import { ProductContent } from "../Product/ProductInterface";
 import BusketItem from "./BusketItem/BusketItem";
+
 const initialProduct: ProductContent = {
   title:
     " 2020 Apple iPad (10.2-inch, Wi-Fi, 32GB) - Space Gray (8th Generation)",
@@ -17,6 +19,20 @@ const initialProduct: ProductContent = {
 };
 
 function Busket() {
+  const [checked, setChecked] = useState([-1]);
+  const [cost, setCost] = useState(0);
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
   return (
     <div className="busket">
       <div className="busket__cart">
@@ -25,20 +41,28 @@ function Busket() {
         </div>
         <Divider />
         <List component="nav">
-          <ListItem>
-            <BusketItem {...initialProduct}></BusketItem>
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <BusketItem {...initialProduct}></BusketItem>
-          </ListItem>
+          {[0, 1, 2, 3].map((value) => {
+            return (
+              <ListItem key={value} onClick={handleToggle(value)}>
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={checked.indexOf(value) !== -1}
+                    tabIndex={-1}
+                    disableRipple
+                  />
+                </ListItemIcon>
+                <BusketItem {...initialProduct}></BusketItem>
+              </ListItem>
+            );
+          })}
         </List>
         <Divider />
       </div>
 
       <div className="busket__total">
         <p>
-          Subtotal (1 item): <strong> $10.99</strong>
+          Subtotal (1 item): <strong> ${cost}</strong>
         </p>
         <div className="busket__btn">
           <Button variant="contained">proceed to checout</Button>
