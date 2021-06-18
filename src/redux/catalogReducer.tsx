d870340f8670dfd9721f2f19016de6e04e0f6c4d
ssemenidov <23sergey03@gmail.com>
2021-06-18 13:02:14 +0800

    new catalog content

--- src/redux/catalogReducer.tsx
+++ src/redux/catalogReducer.tsx
@@ -11,24 +11,22 @@ const initialState = {
     },
     {
       title:
-        " 2020 Apple iPad (10.2-inch, Wi-Fi, 32GB) - Space Gray (8th Generation)",
-      cost: 629.99,
+        " Apple iPhone XS Max, 64GB, Gold - Fully Unlocked (Renewed Premium)",
+      cost: 429.99,
       rate: 3,
-      url: "https://m.media-amazon.com/images/I/71gOkVA6-eL._AC_UL480_FMwebp_QL65_.jpg",
+      url: "https://m.media-amazon.com/images/I/810JGfh-38L._AC_UY327_FMwebp_QL65_.jpg",
     },
     {
-      title:
-        " 2020 Apple iPad (10.2-inch, Wi-Fi, 32GB) - Space Gray (8th Generation)",
-      cost: 629.99,
+      title: "Apple iPhone 11, 64GB, White - Fully Unlocked (Renewed Premium)",
+      cost: 129.99,
       rate: 3,
-      url: "https://m.media-amazon.com/images/I/71gOkVA6-eL._AC_UL480_FMwebp_QL65_.jpg",
+      url: "https://m.media-amazon.com/images/I/41C-k1iuJNS._AC_UY327_FMwebp_QL65_.jpg",
     },
     {
-      title:
-        " 2020 Apple iPad (10.2-inch, Wi-Fi, 32GB) - Space Gray (8th Generation)",
-      cost: 629.99,
+      title: " Apple iPhone 11, 64GB, Green - Fully Unlocked (Renewed Premium)",
+      cost: 329.99,
       rate: 3,
-      url: "https://m.media-amazon.com/images/I/71gOkVA6-eL._AC_UL480_FMwebp_QL65_.jpg",
+      url: "https://m.media-amazon.com/images/I/71EEY8pR-VS._AC_UY327_FMwebp_QL65_.jpg",
     },
     {
       title:
