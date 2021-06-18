b0b50554c2d9d2703e0b7a97e85c22a74420e06a
ssemenidov <23sergey03@gmail.com>
2021-06-18 13:49:06 +0800

    busket delete styles

--- src/components/Busket/BusketItem/BusketItem.tsx
+++ src/components/Busket/BusketItem/BusketItem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ProductContent } from "../../../Interfaces";
 import "./BusketItem.css";
-import Button from "@material-ui/core/Button";
+import { Button, Typography } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { DeleteProduct } from "../../../redux/actions";
 export interface DeleteClick {
@@ -12,19 +12,21 @@ function BusketItem({ product, deleteClick }: DeleteClick) {
   const { title, cost, rate, url } = product;
   return (
     <div className="busketItem">
-      <img src={url} alt="" className="busketItem__img" />
-      <div className="busketItem__content">
-        <div className="busketItem__title">{title}</div>
-        <div className="busketItem__btn">
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => {
-              deleteClick();
-            }}
-          >
-            Delete
-          </Button>
+      <div className="busketItem__left">
+        <img src={url} alt="" className="busketItem__img" />
+        <div className="busketItem__content">
+          <div className="busketItem__title">{title}</div>
+          <div className="busketItem__btn">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                deleteClick();
+              }}
+            >
+              Delete
+            </Button>
+          </div>
         </div>
       </div>
 
