2a451a76250d015448601e3a0a883c11c53a93d1
ssemenidov <23sergey03@gmail.com>
2021-06-25 16:49:18 +0800

    server setup

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,21 @@
 const functions = require("firebase-functions");
-const express=require("express")
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+    // eslint-disable-next-line max-len
+    "sk_test_51J5814KHWr3E9rCy7osCC2feFtWWk8Uz2OneMHlEEbWkwKgj5apumGVSykO1A1U3y9Au4JHAdMUPfxDfytp7zubG00QDM9xnJD"
+);
+// -App config
+const app = express();
+
+// -Middlewares
+app.use(cors({origin: true}));
+app.use(express.json());
+
+
+// - API routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
+app.get("/qazi", (req, res) => res.status(200).send("hello qazi"));
+// - Listen commands
+exports.api= functions.https.onRequest(app);
+// http://localhost:5001/clone-ce035/us-central1/api
